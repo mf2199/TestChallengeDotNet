95899bcb3d41bb7d1699f32c4547586e4f49f150
mf2199 <mf2199@columbia.edu>
2019-10-31 15:02:33 +0300

    Initial version

---
+++ PhoneBookTestApp/PhoneBookTestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookTestApp
+{
+    class Program
+    {
+        private PhoneBook phonebook = new PhoneBook();
+        static void Main(string[] args)
+        {
+            try
+            {
+                DatabaseUtil.initializeDatabase();
+                /* TODO: create person objects and put them in the PhoneBook and the Database
+                * Sam Rothwell, (999) 123-4567, 1242 Morcosse Dr, Jamestown, MN
+                * Clara Tzetkin, (812) 555-1255, 853 Broadway, Hollow, WI
+                */
+
+                // TODO: print the phonebook out to System.out
+                // TODO: find Clara Tzetkin and print out just this entry
+                // TODO: insert the new person objects into the database
+            }
+            finally
+            {
+                DatabaseUtil.CleanUp();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+TestChallengeDotnet - a test challenge for interview follow-ups
+===============================================================
+
+The purpose of this test is to picture your capabilities as a C#/.NET developer. The main goal is spelled out in the Main method of Program.cs file.
+
+- Implement the TODO comments to the best of your ability.
+
+- In addition, you should be looking at how to improve this program. The DatabaseUtil class doesn't need to be significantly refactored. But you may do so, as long as you preserve it's functionality.
+
+- Fork this project, make your changes, then send us back the url.
+
+This test should normally take no more than a few hours. You WILL be given another (quite different) test if you interview here, so please don't try cheating.
+
+Good luck!
